865d1e202f5006fd8edb316ca36e8dd97b022232
Abul Hossain <mdabulhossain780@gmail.com>
2022-08-10 23:44:30 +0600

    feat: antd design and styled-component configuration added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const withAntdLess = require('next-plugin-antd-less');
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const nextConfig = withAntdLess({
   reactStrictMode: true,
   swcMinify: true,
-}
+  lessVarsFilePath: './styles/variables.less',
+  compiler: {
+    styledComponents: true,
+  },
+  webpack(config) {
+    return config;
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
