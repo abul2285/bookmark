337e4eb428f0ae579b7f65efe0d3fda077120a90
Abul Hossain <mdabulhossain780@gmail.com>
2022-08-12 23:25:59 +0600

    refactor component for using with shield component

--- pages/register.js
+++ pages/register.js
@@ -1,35 +1,17 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button, Col, Form, Input, Row, Typography } from 'antd';
 
-import useAuth from '../hooks/useAuth';
-import { getUsername } from '../utils/storage';
 import useRegister from '../hooks/useRegister';
 
 const { Text } = Typography;
 
 export default function Register() {
-  const router = useRouter();
   const { register } = useRegister();
   const [loading, setLoading] = useState();
-  const [fetching, setFetching] = useState(true);
-
-  const { fetchProfile } = useAuth();
 
   const handleSubmit = (values) => {
     register(values, setLoading);
   };
-
-  useEffect(() => {
-    (async function () {
-      const username = getUsername();
-      if (!username) return setFetching(false);
-      const res = await fetchProfile(username, setFetching);
-      if (res?.username) router.push('/');
-    })();
-  }, [fetchProfile, router]);
-
-  if (fetching) return <p>Loading...</p>;
 
   return (
     <Row style={{ marginTop: 100 }}>
